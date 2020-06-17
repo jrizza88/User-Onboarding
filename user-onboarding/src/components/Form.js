434a9c1165a7b50303d603970f72cc533470e4db
Jamar Torres <Jetorres88@gmail.com>
2020-06-16 22:19:56 -0400

    set up handleChange function and set up the name input in the form.

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,8 +2,33 @@ import React, {useState} from 'react';
 
 
 const Form = () => {
+    // create a form to onboard a new user to the system. 
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+        TermsOfService: ""
+    })
+
+    const handleChange = e => {
+        console.log('target name: ', e.target.name)
+        console.log('targer value: ', e.target.value)
+        setForm({ ...form, [e.target.name]: e.target.value})
+    }
+
     return (
-        <div>Form</div>
+        <div>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" 
+                  id="name" 
+                  name="name" 
+                  placeHolder="Enter Name"
+                   onChange={handleChange} 
+                   value={form.name} 
+                />
+            </form>
+        </div>
     )
 }
 export default Form;
